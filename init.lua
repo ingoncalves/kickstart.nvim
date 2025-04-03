0f9de9952a77274f2ba221c8f53b9b5c6a9c6829
Guilherme Goncalves <inacio.guilherme@gmail.com>
2025-04-03 12:54:44 -0300

    Added Control+p and Control+f mapping aliases to telescope

--- init.lua
+++ init.lua
@@ -456,6 +456,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Search Files alias [Control + p]' })
+      vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Search by Grep alias [Control + f]' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
