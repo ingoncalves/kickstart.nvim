2a975e9e54045de2d6441da6613dbad4b2e5eb85
Guilherme Gonçalves <gui@siteimprove.com>
2026-02-02 13:19:02 -0300

    Added settings customization for neo-tree

--- init.lua
+++ init.lua
@@ -1099,5 +1099,7 @@ require('lazy').setup({
   },
 })
 
+require 'custom.settings'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/settings/init.lua
@@ -0,0 +1,1 @@
+require 'custom.settings.neo-tree'
