e92cd993271f1b206e39bc667ab14c6dbd0d9d7e
Guilherme Gonçalves <gui@siteimprove.com>
2026-02-02 13:20:10 -0300

    Replaced custom dap with kickstat one

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,9 +25,5 @@ return {
     config = function()
       require('treesj').setup()
     end,
-  },
-  {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio'}
-  }, -- Debugging
+  }
 }
